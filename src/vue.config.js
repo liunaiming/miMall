a86611a0e01a5921c308edea54a0df4ed67ee5d5
liunaiming <liunaiming1993@163.com>
2020-06-24 00:31:58 +0800

    项目目录变更

---
+++ src/vue.config.js
@@ -0,0 +1,17 @@
+module.exports = { 
+    devServer:{
+        host:'localhost',
+        port:8080,
+        proxy:{
+            'activity':{
+                target:'https//www.imooc.com',
+                changeOrigin:true
+            }
+        },
+        PathRewrite:{
+            '/activity':'/activity'
+        }
+    }
+    
+
+}
